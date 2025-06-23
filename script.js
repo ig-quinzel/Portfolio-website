4bea72fc68440b59d19989a7c493e0efc68eb597
ig-quinzel <nandanak267@gmail.com>
2025-06-23 10:39:33 +0530

    Updated project pages and index.html

---
+++ script.js
@@ -0,0 +1,25 @@
+
+    var tablinks=document.getElementsByClassName("tab-links");
+    var tabcontents=document.getElementsByClassName("tab-contents");
+    function opentab(tabname){
+        for(tablink of tablinks){
+            tablink.classList.remove("active-link");
+        }
+        for(tabcontent of tabcontents){
+            tabcontent.classList.remove("active-tab");
+        }
+        event.currentTarget.classList.add("active-link");
+        document.getElementById(tabname).classList.add("active-tab");
+    } 
+  function toggleProjects() {
+    const moreProjects = document.getElementById("more-projects");
+    const btn = document.getElementById("showMoreBtn");
+
+    if (moreProjects.style.display === "none") {
+      moreProjects.style.display = "grid";
+      btn.innerText = "Show Less";
+    } else {
+      moreProjects.style.display = "none";
+      btn.innerText = "Show More";
+    }
+  }
